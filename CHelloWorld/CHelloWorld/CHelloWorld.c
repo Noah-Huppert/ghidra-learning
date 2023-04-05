9d9fb6fce01cf5dfd620f4f50edfe5614eb5a449
Noah Huppert <3323504+Noah-Huppert@users.noreply.github.com>
2023-04-05 00:28:44 -0400

    created hello world and trying to inspect it

---
+++ CHelloWorld/CHelloWorld/CHelloWorld.c
@@ -0,0 +1,10 @@
+// CHelloWorld.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World!\n");
+    return 0;
+}
